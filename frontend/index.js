3b32c9c3a689970aafbca45953b93ab3ad87ab7c
Jiafeng-Li95 <jeff1106593594@gmail.com>
2021-11-12 22:01:13 -0500

    update js function
    update js button eventlistener

--- frontend/index.js
+++ frontend/index.js
@@ -1,2 +1,13 @@
+document.getElementById("max").addEventListener('click', max);
+document.getElementById("min").addEventListener('click', min);
+document.getElementById("go").addEventListener('click', go);
 
-
+function go() {
+    alert("Go!");
+}
+function min() {
+    alert("min");
+}
+function max() {
+    alert("Max");
+}
