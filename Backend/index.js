cb83cd89df3b460a6a922bba31c383c7ba41f025
Qiming-C <49624964+Qiming-C@users.noreply.github.com>
2021-11-13 12:54:56 -0500

    able to fetch elevation

--- Backend/index.js
+++ Backend/index.js
@@ -22,4 +22,5 @@ app.get("/", (req, res) => {
 app.listen(port, async () => {
   //generate the graph first
   let graph = await mapService.generateGraph(mySettings);
+  mapService.getElevations();
 });
--- Backend/map/mapService.js
+++ Backend/map/mapService.js
@@ -1,16 +1,19 @@
 const graphFromOsm = require("graph-from-osm");
 const haversine = require("haversine");
+const fetch = (...args) =>
+  import("node-fetch").then(({ default: fetch }) => fetch(...args));
 let createGraph = require("ngraph.graph");
 
+//actual graph we are constructing
 let graph = createGraph();
 //g for holding the original parse data from osm
 let g = null;
 
 //covert OSM xml data into json like graph structure
+//and convert to actual graph data structure
 async function generateGraph(settings) {
   const osmData = await graphFromOsm.getOsmData(settings); // Import OSM raw data
   g = graphFromOsm.osmDataToGraph(osmData); // Here is your graph
-  console.log(g.features);
   //filter out the geometry type with point which is node
   let points = g.features.filter((obj) => obj.geometry.type === "Point");
   let ways = g.features.filter((obj) => obj.geometry.type === "LineString");
@@ -44,4 +47,26 @@ async function generateGraph(settings) {
   return graph;
 }
 
-module.exports = { generateGraph };
+async function getElevations() {
+  let body = {
+    locations: [],
+  };
+
+  graph.forEachNode((node) => {
+    body.locations.push({
+      latitude: node.data.coordinates[1],
+      longitude: node.data.coordinates[0],
+    });
+  });
+
+  //send post request to retrieve elevation information
+  let response = await fetch("https://api.open-elevation.com/api/v1/lookup", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(body),
+  });
+
+  return await response.json();
+}
+
+module.exports = { generateGraph, getElevations };
