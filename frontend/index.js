55ad651c18a8ab6ba32edf78aab51ab54a916166
Jiafeng-Li95 <jeff1106593594@gmail.com>
2021-11-13 11:02:32 -0500

    Update index.js
    Add eventListener-Go, min, max.

--- frontend/index.js
+++ frontend/index.js
@@ -3,13 +3,30 @@ document.getElementById("min").addEventListener('click', min);
 document.getElementById("go").addEventListener('click', go);
 
 function go() {
-    alert("Go!");
+    //get latlngs
+    let source_l1, source_l2, des_l1, des_l2;
+
+    let words = document.getElementById("source").value.replace("LatLng(", "").split(",");
+    source_l1 = words[0].replace(/\s/g, "");
+    source_l2 = words[1].replace(/\s/g, "").replace(")", "");
+    words = document.getElementById("destination").value.replace("LatLng(", "").split(",");
+    des_l1 = words[0].replace(/\s/g, "");
+    des_l2 = words[1].replace(/\s/g, "").replace(")", "");
+    console.log("Source : " + source_l1 + " " + source_l2 + "\n" + "Destination : " + des_l1 + " " + des_l2);
+
+    //short %
+    let sliderValue = document.getElementById("myRange").value;
+    console.log(sliderValue);
+
+
 }
 function min() {
-    alert("min");
+    document.getElementById("myRange").value = 100;
+    document.getElementById("sliderValue").textContent = document.getElementById("myRange").value + "%";
 }
 function max() {
-    alert("Max");
+    document.getElementById("myRange").value = 200;
+    document.getElementById("sliderValue").textContent = document.getElementById("myRange").value + "%";
 }
 document.getElementById("myRange").addEventListener("change", function () {
     document.getElementById("sliderValue").textContent = document.getElementById("myRange").value + "%";
