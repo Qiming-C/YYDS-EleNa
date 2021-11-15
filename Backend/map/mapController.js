21d9d06bbb5ad11ad77e942ece9f7fd05b770b07
Qiming-C <49624964+Qiming-C@users.noreply.github.com>
2021-11-15 00:09:37 -0500

    DFS for finding all paths

--- Backend/map/mapController.js
+++ Backend/map/mapController.js
@@ -28,8 +28,9 @@ const mapService = require("./mapService");
  * NOTES: remember to cat data to proper type 
  */
 mapRouter.post("/max", async (req, res) => {
-  mapService.checkGraph();
-  mapService.findShortestPath();
+  //mapService.findShortestPath();
+  let list = mapService.findAllPaths(2, 20);
+  console.log(list);
   res.send("Processed");
 });
 
--- Backend/map/mapService.js
+++ Backend/map/mapService.js
@@ -8,6 +8,7 @@ function checkGraph() {
 }
 
 //TODO: A* algorithm
+//FIXME: need to sync the progress otherwise when user click too quick while server is up will cause some unfinish graph issue
 function findShortestPath() {
   //73048441
   let fromNodeId = 2;
@@ -27,36 +28,63 @@ function findShortestPath() {
 //TODO: DFS for finding all the paths
 function findAllPaths(source, target) {
   //boolean type for checking if node is visisted
-  let isVisited = [];
+  let verticeCount = 0;
+  graph.forEachNode(function (node) {
+    verticeCount++;
+  });
+  let isVisited = new Array(verticeCount).fill(false);
   //store the path
   let pathList = [];
+  let final = [];
+  //fetch sourced node and target node first
+  let s = graph.getNode(source);
+  let t = graph.getNode(target);
 
   //add source node to path
-  pathList.push(source);
+  pathList.push(s);
 
   //call util dfs
-  DFSutils(source, target, isVisited, pathList);
+  DFSUtils(s, t, isVisited, pathList, final);
+  return final;
 }
 
-function DFSUtils(source, target, isVisited, pathList) {
+function DFSUtils(source, target, isVisited, pathList, final) {
   //check if two node are equal
-  // if (source.id === target.id) {
-  //   console.log(pathList);
-  //   return;
-  // }
+  if (source === target) {
+    final.push([...pathList]);
+    return;
+  }
+  // mark the current node to be visited
+  isVisited[source.id] = true;
 
-  //mark the current node to be visited
-  //isVisited[source.id] = true;
+  // get the adjacent list
+  let adjacent = [];
+  //TRUE indicates we only want the outgoing edge
+  graph.forEachLinkedNode(
+    source.id,
+    (linkedNode, link) => {
+      adjacent.push(linkedNode);
+    },
+    true
+  );
 
-  //recursive for all the neighbors
-  graph.forEachLinkedNode(6, (linkedNode, link) => {
-    console.log("Connected node:", linkedNode.id, linkedNode.data);
-    console.dir(link);
-  });
+  for (let i = 0; i < adjacent.length; i++) {
+    if (!isVisited[adjacent[i]]) {
+      //store current node to pathList
+      pathList.push(adjacent[i]);
+      DFSUtils(adjacent[i], target, isVisited, pathList, final);
+
+      //backtracking
+      pathList.splice(pathList.indexOf(adjacent[i]), 1);
+    }
+  }
+
+  //mark the current node to unvisited
+  isVisited[source.id] = false;
 }
 
 //TODO: Dijkstra algorithm finding the shortest path
 
 //TODO: compute the shortest path with elevation gain awareness
 
-module.exports = { checkGraph, findShortestPath, DFSUtils };
+module.exports = { checkGraph, findShortestPath, DFSUtils, findAllPaths };
