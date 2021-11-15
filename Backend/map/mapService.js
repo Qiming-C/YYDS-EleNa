09ffa6002d850a49b0d98c4c1cc7e4852cb76885
Qiming-C <49624964+Qiming-C@users.noreply.github.com>
2021-11-14 22:57:23 -0500

    found the bug of override variable

--- Backend/map/mapService.js
+++ Backend/map/mapService.js
@@ -19,17 +19,44 @@ function findShortestPath() {
       return link.data.distance;
     },
   });
-  path = pathFinder.find(fromNodeId, toNodeId);
-  console.log(path);
-  graph.forEachLink((link) => {
-    console.log(link);
-  });
+
+  //path is going backward order
+  let shortestPath = pathFinder.find(fromNodeId, toNodeId);
 }
 
 //TODO: DFS for finding all the paths
+function findAllPaths(source, target) {
+  //boolean type for checking if node is visisted
+  let isVisited = [];
+  //store the path
+  let pathList = [];
+
+  //add source node to path
+  pathList.push(source);
+
+  //call util dfs
+  DFSutils(source, target, isVisited, pathList);
+}
+
+function DFSUtils(source, target, isVisited, pathList) {
+  //check if two node are equal
+  // if (source.id === target.id) {
+  //   console.log(pathList);
+  //   return;
+  // }
+
+  //mark the current node to be visited
+  //isVisited[source.id] = true;
+
+  //recursive for all the neighbors
+  graph.forEachLinkedNode(6, (linkedNode, link) => {
+    console.log("Connected node:", linkedNode.id, linkedNode.data);
+    console.dir(link);
+  });
+}
 
 //TODO: Dijkstra algorithm finding the shortest path
 
 //TODO: compute the shortest path with elevation gain awareness
 
-module.exports = { checkGraph, findShortestPath };
+module.exports = { checkGraph, findShortestPath, DFSUtils };
