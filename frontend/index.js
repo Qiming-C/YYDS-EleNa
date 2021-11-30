75d4569a7654c6bddd30137d2dfc3ac4ae3952e5
hilshong2580 <63128458+hilshong2580@users.noreply.github.com>
2021-11-29 22:09:34 -0500

    Update to change Route detail's title

--- frontend/index.js
+++ frontend/index.js
@@ -17,7 +17,8 @@ function go() {
   if (
     document.getElementById("source").value === "" ||
     document.getElementById("destination").value === "" ||
-    type === null || walkOrCar == null
+    type === null ||
+    walkOrCar == null
   ) {
     alert("Need more information");
   } else {
@@ -42,11 +43,11 @@ function go() {
     des_l1 = words[0].replace(/\s/g, "");
     des_l2 = words[1].replace(/\s/g, "").replace(")", "");
 
-    //short 
+    //short
     let sliderValue = document.getElementById("myRange").value;
     sliderValue = (sliderValue - 100) / 100;
 
-    // raw data set 
+    // raw data set
     const raw = {
       start: {
         coordinates: [source_l2, source_l1],
@@ -76,41 +77,55 @@ async function postData(JSONData, maxOrMin, walkOrCar) {
   if (walkOrCar !== "") {
     walkOrCar += "/";
   }
-  fetch("http://localhost:3000/api/map/" + walkOrCar + maxOrMin, requestOptions).then(
-    async (response) => {
-      if (response.status === 200) {
-        const data = (await response.json());
-        console.log(data);
-        if (data.path.length === 0) {
-          alert("No path");
-          document.getElementById("table_distance").innerHTML = "";
-          document.getElementById("table_elevation").innerHTML = "";
-          document.getElementById("table_percentage").innerHTML = "";
-          document.getElementById("table_node").innerHTML = "";
-
-        } else {
-          let latlngs = [
-            [JSONData.start.coordinates[1], JSONData.start.coordinates[0]]
-          ];
-          for (let i = 0; i < data.path.length; i++) {
-            latlngs.push([data.path[i].data.coordinates[0], data.path[i].data.coordinates[1]]);
-          }
-          latlngs.push([JSONData.end.coordinates[1], JSONData.end.coordinates[0]]);
-
-          polyline = L.polyline(latlngs, { color: 'blue' }).addTo(map);
-
-          //render the route info to table
-          document.getElementById("table_distance").innerHTML = (Math.round(data.distance)/1000).toString()+" mile(s)";
-          document.getElementById("table_elevation").innerHTML = (Math.round(data.elevationGain)).toString()+" meter(s)";
-          document.getElementById("table_percentage").innerHTML = "X %";
-          document.getElementById("table_node").innerHTML = (data.path.length+2).toString()+" node(s)";
-
+  fetch(
+    "http://localhost:3000/api/map/" + walkOrCar + maxOrMin,
+    requestOptions
+  ).then(async (response) => {
+    if (response.status === 200) {
+      const data = await response.json();
+      console.log(data);
+      if (data.path.length === 0) {
+        alert("No path");
+        document.getElementById("table_distance").innerHTML = "";
+        document.getElementById("table_elevation").innerHTML = "";
+        document.getElementById("table_percentage").innerHTML = "";
+        document.getElementById("table_node").innerHTML = "";
+      } else {
+        let latlngs = [
+          [JSONData.start.coordinates[1], JSONData.start.coordinates[0]],
+        ];
+        for (let i = 0; i < data.path.length; i++) {
+          latlngs.push([
+            data.path[i].data.coordinates[0],
+            data.path[i].data.coordinates[1],
+          ]);
         }
+        latlngs.push([
+          JSONData.end.coordinates[1],
+          JSONData.end.coordinates[0],
+        ]);
 
+        polyline = L.polyline(latlngs, { color: "blue" }).addTo(map);
+
+        //render the route info to table
+        let type = "";
+        if (walkOrCar == "") {
+          type = "car";
+        } else {
+          type = walkOrCar.replace("/", "");
+        }
+        document.getElementById("Route_title").innerHTML =
+          maxOrMin + " route by " + type + ".";
+        document.getElementById("table_distance").innerHTML =
+          (Math.round(data.distance) / 1000).toString() + " mile(s)";
+        document.getElementById("table_elevation").innerHTML =
+          Math.round(data.elevationGain).toString() + " meter(s)";
+        document.getElementById("table_percentage").innerHTML = "X %";
+        document.getElementById("table_node").innerHTML =
+          (data.path.length + 2).toString() + " node(s)";
       }
-      else console.log("fail in index");
-    }
-  );
+    } else console.log("fail in index");
+  });
 }
 
 function min() {
@@ -179,10 +194,10 @@ function buildMap() {
     [42.373775, -72.515408],
     [42.402095, -72.515408],
     [42.402095, -72.541988],
-    [42.373775, -72.541988]
+    [42.373775, -72.541988],
   ];
 
-  polyline = L.polyline(latlngs, { color: 'red' }).addTo(map);
+  polyline = L.polyline(latlngs, { color: "red" }).addTo(map);
 
   let words = document
     .getElementById("source")
@@ -197,18 +212,19 @@ function buildMap() {
   des_l1 = words[0].replace(/\s/g, "");
   des_l2 = words[1].replace(/\s/g, "").replace(")", "");
 
-
   //event listener when click the map
   let markerOutset = null;
   let markerDestination = null;
 
   var violetIcon = new L.Icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconUrl:
+      "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png",
+    shadowUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
-    shadowSize: [41, 41]
+    shadowSize: [41, 41],
   });
 
   markerOutset = L.marker([source_l1, source_l2], { icon: violetIcon })
@@ -217,12 +233,14 @@ function buildMap() {
     .openPopup();
 
   var redIcon = new L.Icon({
-    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
-    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconUrl:
+      "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png",
+    shadowUrl:
+      "https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png",
     iconSize: [25, 41],
     iconAnchor: [12, 41],
     popupAnchor: [1, -34],
-    shadowSize: [41, 41]
+    shadowSize: [41, 41],
   });
 
   markerDestination = L.marker([des_l1, des_l2], { icon: redIcon })
@@ -234,7 +252,6 @@ function buildMap() {
   //source or destination record counter
   var counter = 1;
 
-
   //get the OSM ID by lat and lon
   function get_id(lat, lon, point) {
     let requestOptions = {
@@ -243,7 +260,10 @@ function buildMap() {
     };
 
     fetch(
-      "https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=" + lat + "&lon=" + lon,
+      "https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=" +
+        lat +
+        "&lon=" +
+        lon,
       requestOptions
     )
       .then((response) => response.text())
@@ -269,22 +289,21 @@ function buildMap() {
 
     //set the turns of filling the latlng to the source or destination input fields
     if (counter == 1) {
-
       document.getElementById("source").value = e.latlng.toString();
       counter += 1;
 
       if (markerOutset !== null) {
         map.removeLayer(markerOutset);
       }
-      markerOutset = L.marker([parseFloat(latlng1), parseFloat(latlng2)], { icon: violetIcon })
+      markerOutset = L.marker([parseFloat(latlng1), parseFloat(latlng2)], {
+        icon: violetIcon,
+      })
         .addTo(map)
         .bindPopup("outset!")
         .openPopup();
 
       get_id(latlng1, latlng2, "outset");
-
     } else {
-
       document.getElementById("destination").value = e.latlng.toString();
       counter -= 1;
 
@@ -292,7 +311,9 @@ function buildMap() {
         map.removeLayer(markerDestination);
       }
 
-      markerDestination = L.marker([parseFloat(latlng1), parseFloat(latlng2)], { icon: redIcon })
+      markerDestination = L.marker([parseFloat(latlng1), parseFloat(latlng2)], {
+        icon: redIcon,
+      })
         .addTo(map)
         .bindPopup("Destination!")
         .openPopup();
