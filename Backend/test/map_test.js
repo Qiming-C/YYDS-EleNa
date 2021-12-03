5236d5a1c35fc0ae370811ab768afe3544bf0773
Qiming-C <49624964+Qiming-C@users.noreply.github.com>
2021-12-03 12:41:22 -0500

    vehicle map test cases

--- Backend/test/map_test.js
+++ Backend/test/map_test.js
@@ -1,23 +1,24 @@
 /* eslint-disable no-undef */
 let expect = require("chai").expect;
 let { CAR_settings } = require("../map/enum");
-let { generateGraph } = require("../map/mapModel");
-let createGraph = require("ngraph.graph");
+let carService = require("../map/mapService");
 
-let graph = createGraph();
-describe("car map", async function () {
+describe("vehicle navigation map features", async function () {
   before(async function () {
-    await generateGraph(CAR_settings, graph);
+    await carService.init(CAR_settings);
   });
 
-  describe("number of nodes", async function () {
+  describe("Number of nodes", async function () {
     it("should have the correct number of nodes of 1100", async function () {
-      let i = 0;
-      graph.forEachNode(() => {
-        i++;
-      });
+      expect(carService.checkGraph()).equal(1100);
+    });
+  });
 
-      expect(i).equal(1100);
+  describe("find the shortest path", async function () {
+    let shortestPath = carService.findShortestPath(20, 10);
+
+    it("should return the shortest path length ", async function () {
+      expect(shortestPath.length).to.be.equal(15);
     });
   });
 });
