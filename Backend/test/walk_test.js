17d1e0cb3d48acf40c4696fafb0fd195afdc054f
Qiming-C <49624964+Qiming-C@users.noreply.github.com>
2021-12-03 22:59:36 -0500

    mapService and walkService test suites completed

--- Backend/test/walk_test.js
+++ Backend/test/walk_test.js
@@ -20,5 +20,78 @@ describe("walk/bike/run navigation map features", async function () {
     it("should return the shortest path length ", async function () {
       expect(walkService.findShortestPath(20, 10).length).to.be.equal(28);
     });
+
+    it("should return 14 the possible paths", function () {
+      expect(walkService.findAllPaths(20, 10, 28).length).to.be.equal(14);
+    });
+  });
+
+  describe("walk request with short distance max elevation gain", function () {
+    let response;
+    before(function () {
+      let source = {
+        lat: 42.396127,
+        lon: -72.529714,
+      };
+      let target = {
+        lat: 42.396571,
+        lon: -72.527653,
+      };
+      response = walkService.calculateRequestPath(source, target, 1.5, true);
+    });
+
+    it("should process the request with not null", function () {
+      expect(response).not.equal(null);
+    });
+
+    it("should process the distance under 150% percentage limit", function () {
+      expect(response.distance).lessThan(response.shortestDistance * 1.5);
+    });
+  });
+
+  describe("walk request with short distance min elevation gain", function () {
+    let response;
+    before(function () {
+      let source = {
+        lat: 42.396127,
+        lon: -72.529714,
+      };
+      let target = {
+        lat: 42.396571,
+        lon: -72.527653,
+      };
+      response = walkService.calculateRequestPath(source, target, 1.5, false);
+    });
+
+    it("should process the request with not null", function () {
+      expect(response).not.equal(null);
+    });
+
+    it("should process the distance under 150% percentage limit", function () {
+      expect(response.distance).lessThan(response.shortestDistance * 1.5);
+    });
+  });
+
+  describe("walk request with long distance", function () {
+    let response;
+    before(function () {
+      let source = {
+        lat: 42.3997,
+        lon: -72.5298,
+      };
+      let target = {
+        lat: 42.3959,
+        lon: -72.5243,
+      };
+      response = walkService.calculateRequestPath(source, target, 1.5, true);
+    });
+
+    it("should process the request with not null", function () {
+      expect(response).not.equal(null);
+    });
+
+    it("should process the distance under 150% percentage limit", function () {
+      expect(response.distance).lessThan(response.shortestDistance * 1.5);
+    });
   });
 });
